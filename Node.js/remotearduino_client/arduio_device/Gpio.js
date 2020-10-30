38a7696a546670a3de0133914368b283e9d2030b
poruruba <44470781+poruruba@users.noreply.github.com>
2020-10-30 09:00:54 +0900

    Update Gpio.js

--- Node.js/remotearduino_client/arduio_device/Gpio.js
+++ Node.js/remotearduino_client/arduio_device/Gpio.js
@@ -4,6 +4,18 @@ class Gpio{
   constructor(arduino){
    this.arduino = arduino;
    this.device_type = "Gpio";
+    
+   this.LOW = 0x0;
+   this.HIGH = 0x1;
+
+   this.INPUT = 0x01;
+   this.OUTPUT = 0x02;
+   this.PULLUP = 0x04;
+   this.INPUT_PULLUP = 0x05;
+   this.PULLDOWN = 0x08;
+   this.INPUT_PULLDOWN = 0x09;
+   this.OPEN_DRAIN = 0x10;
+   this.OUTPUT_OPEN_DRAIN = 0x12;
   }
 
   async pinMode(pin, mode){
